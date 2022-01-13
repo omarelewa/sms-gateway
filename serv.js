5550d5bb8d81175485b403014cb041cfc5c84dc5
omar <elewa97@aucegypt.edu>
2022-01-13 23:23:48 +0200

    Working APIs

--- serv.js
+++ serv.js
@@ -27,13 +27,39 @@ srv.get("/sendSMS",
     console.log(q.phone, q.message);
 
         let sql = "INSERT INTO smsQ.sms_table (phone,body) VALUES(" + q.phone + ", \""+ q.message + "\");";
-        // sql = sql + q.phone + ", " + q.message + ")";
     mysqlcon.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
         res.end("1");
     });
 });
+
+srv.get("/getSMS",
+    function (req,
+              res) {
+    mysqlcon.query("select * from sms_table where (ts=(select min(ts) from sms_table where sent = 0))",
+        function (err, result, fields) {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+});
+
+srv.get("/smsSent",
+    function (req,
+              res) {
+        q = url.parse(req.url,
+            true).query;
+        console.log(q.id);
+
+        let sql = "UPDATE sms_table SET sent = 1 WHERE id ="+q.id;
+        mysqlcon.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 message sent");
+            let m = q.id + ": sent"
+            res.send(m);
+        });
+    });
 
 srv.listen(3000, function () {
     console.log("Server is listening on port 3000.");
